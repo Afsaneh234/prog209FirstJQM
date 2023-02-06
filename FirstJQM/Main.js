99445154fd6cc8d4744c8de7cda6c12e3a046333
YaCaiBC <122326064+YaCaiBC@users.noreply.github.com>
2023-02-05 17:36:32 -0800

    fourth change

--- FirstJQM/Main.js
+++ FirstJQM/Main.js
@@ -1,51 +1,12 @@
-const body = document.getElementsByTagName('body')[0];
 
-body.append(page1);
+    document.getElementById("btn").addEventListener("click",menu )
+    function menu() {
+        var p = document.createElement("p");
+        var t = document.createTextNode("Bring a large pot of lightly salted water to a boil. Cook udon in boiling water, stirring occasionally, until noodles are tender yet firm to the bite, 10 to 12 minutes. Drain. Meanwhile, combine chicken broth, soy sauce, peanut butter, ginger, honey, garlic, and chili paste in a medium saucepan. Cook and stir over medium heat until peanut butter melts and sauce is heated through. Add drained noodles to peanut butter sauce; toss to coat. Garnish with green onions and peanuts.");
+      p.appendChild(t);
+      document.body.appendChild(p);
+    }
 
-let choices = ["Recipe"];
-choices.forEach((choice) => {
-  const menu = document.createElement('input');
-  menu.type = "radio";
-  let label = document.createElement('label');
-  menu.name = 'interest';
-  menu.id = choice;
-  label.innerText = choice;
-  label.for = choice;
-  let option =document.createElement('div');
-  option.className='option';
-  option.append(label);
-  option.append(menu);
-  output1.append(option);
-})
-page1.append(output1);
-
-let btn = document.createElement('button');
-btn.addEventListener("click", level);
-btn.id ='btn';
-btn.textContent ="click here to find the menu";
-page1.append(btn);
-
-function level(event) {
-   
-let menu = document.querySelector('input[name="interest"]:checked');
-
-let result =document.createElement('div');
-result.id='result';
-  menu =menu.id;
-  if(document.getElementById('result')){
-  document.getElementById('result').remove();
-  }
-
-  let page1 =document.getElementById('page1');
-   body.append(result);
-  
- if(menu ==='Recipe' ) {
-result.textContent ='Udon Noddles';
-     let message1 =['Bring a large pot of lightly salted water to a boil. Cook udon in boiling water, stirring occasionally, until noodles are tender yet firm to the bite, 10 to 12 minutes. Drain. Meanwhile, combine chicken broth, soy sauce, peanut butter, ginger, honey, garlic, and chili paste in a medium saucepan. Cook and stir over medium heat until peanut butter melts and sauce is heated through. Add drained noodles to peanut butter sauce; toss to coat. Garnish with green onions and peanuts.'];
-body.append(message1);
- }
-}
-   
  PersonArray = []; 
 
 function formSubmitEvent() {
